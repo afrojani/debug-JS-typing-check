c4a3a0be34f2b8cbfcd31c63845d39a8a38cdca4
afrojani <sultana.ani2019@gmail.com>
2022-09-15 02:03:50 -0500

    countdown corrected

--- script.js
+++ script.js
@@ -103,30 +103,26 @@ const closeModal = () => {
 const start = () => {
   // If already started, do not start again
   if (startTime) {
-    ;
-
     let count = 3;
     countdownOverlay.style.display = "flex";
 
     const startCountdown = setInterval(() => {
       countdownOverlay.innerHTML = `<h1>${count}</h1>`;
-    })
-  }
 
-  // finished timer
-  if (count === 0) {
-    // -------------- START TYPING -----------------
-    clearInterval();
-    display.addEventListener("keydown", typeController);
-    countdownOverlay.style.display = "flex";
-    display.classList.remove("inactive");
+      // finished timer
+      if (count == 0) {
+        // -------------- START TYPING -----------------
+        document.addEventListener("keydown", typeController);
+        countdownOverlay.style.display = "flex";
+        display.classList.remove("inactive");
 
-    startTime = new Date().getTime();
+        clearInterval(startCountdown);
+        startTime = new Date().getTime();
+      }
+      count--;
+    }, 1000);
   }
-}
-//     count--;
-//   }, 1000);
-// };
+};
 
 // START Countdown
 startBtn.addEventListener("click", start);
